60b6294c88a2dae7146cceab9bba74689b969447
Zibilyonik <34018257+Zibilyonik@users.noreply.github.com>
2019-07-13 18:52:48 +0300

    bin/main.rb added, player mark placement test added

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,5 @@
 require './lib/game'
+require './bin/main'
 
 describe Board do
     describe "#full?" do
@@ -13,9 +14,15 @@ end
 
 describe Player do
     describe "#play" do
-        it "has add a player mark on the board" do
+        it "should add a player mark on the board" do
             board = Board.new
-
+            player1=Player.new
+            input=gets
+            if input > 1 and input < 10 and board[input]!="*"
+              expect(board[input]).to be="X"
+            else
+              expect !board.available.include.to eql(true)
+            end
         end
     end
 end
