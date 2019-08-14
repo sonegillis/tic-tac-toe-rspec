d0848151bb1028c79aa56e8226a7aa9e4b9ff320
Zibilyonik <34018257+Zibilyonik@users.noreply.github.com>
2019-08-14 17:30:38 +0300

    some linter errors fixed

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,24 +2,27 @@ require './lib/game.rb'
 
 
 describe Board do
-    describe "#full?" do
-        it "has to determine of the board is full or not" do
-            board = Board.new
-            expect(board.full?).to eql(false)
-            board.available.clear()
-            expect(board.full?).to eql(true)
-        end
-    end
-    describe "availability" do
-        it "should check if the occupied cell is marked as unavailable" do
-            board = Board.new
-            board.pattern[0,0]="X"
-            board.available.delete(1)
-            expect(board.available).not_to include(1)
-        end
-    end
+
+  describe "#full?" do
+      it "has to determine of the board is full or not" do
+          board = Board.new
+          expect(board.full?).to eql(false)
+          board.available.clear()
+          expect(board.full?).to eql(true)
+      end
+  end
+
+  describe "availability" do
+      it "should check if the occupied cell is marked as unavailable" do
+          board = Board.new
+          board.pattern[0,0]="X"
+          board.available.delete(1)
+          expect(board.available).not_to include(1)
+      end
+  end
 end
 describe Player do
+
   describe "#play" do
     it "places a marker and removes the cell from availables list" do
       board=Board.new
@@ -28,6 +31,7 @@ describe Player do
       expect(board.available).not_to include(1)
     end
   end
+
   describe "#check_win" do
     it "checks if a player won the game" do
       board=Board.new
